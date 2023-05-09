e0b2307a97a86165201193070a3a447c8239babb
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-05-09 03:01:35 -0500

    Update defaultChannels.ps1

--- channelPages/defaultChannels.ps1
+++ channelPages/defaultChannels.ps1
@@ -1,17 +1,9 @@
-	switch ($creator) {
-		"NetworkChuck"               {$channelID = "UC9x0AN7BWHpCDHSm9NiJFJQ"}
-
-		"JohnHammond"                {$channelID = "UCVeW9qkBjo3zosnqUbG7CFw"}
-
-		"Stok"                       {$channelID = "UCQN2DsjnYH60SFBIA6IkNwg"}
-
-		"Hak5"                       {$channelID = "UC3s0BtrBJpwNDaflRSoiieQ"}
-
-		"Nahamsec"                   {$channelID = "UCCZDt7MuC3Hzs6IH4xODLBw"}
-
-		"Zsecurity"                  {$channelID = "UCVPjtOVcnKaSRI8IO3KSetA"}
-		
-		"iamjakoby"                  {$channelID = "UCQUz2mC5Regc63XRWEqD9FA"}		
-
-		Default                      {$channelID = "UCQUz2mC5Regc63XRWEqD9FA"}
-	}
+$channels         = @{
+    NetworkChuck = "UC9x0AN7BWHpCDHSm9NiJFJQ"
+    JohnHammond  = "UCVeW9qkBjo3zosnqUbG7CFw"
+    Stok         = "UCQN2DsjnYH60SFBIA6IkNwg"
+    Hak5         = "UC3s0BtrBJpwNDaflRSoiieQ"
+    Nahamsec     = "UCCZDt7MuC3Hzs6IH4xODLBw"
+    Zsecurity    = "UCVPjtOVcnKaSRI8IO3KSetA"
+    Sasquach     = "UCUoJk48pujh29p8zLsnD5PQ"
+}
