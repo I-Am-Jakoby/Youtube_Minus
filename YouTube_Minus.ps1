ecc8da23fbb5d692fba8060c09825273b3e4d7c6
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-05-10 05:46:42 -0500

    Update YouTube_Minus.ps1

--- YouTube_Minus.ps1
+++ YouTube_Minus.ps1
@@ -1,3 +1,13 @@
+$channels         = @{
+    NetworkChuck = "UC9x0AN7BWHpCDHSm9NiJFJQ"
+    JohnHammond  = "UCVeW9qkBjo3zosnqUbG7CFw"
+    Stok         = "UCQN2DsjnYH60SFBIA6IkNwg"
+    Hak5         = "UC3s0BtrBJpwNDaflRSoiieQ"
+    Nahamsec     = "UCCZDt7MuC3Hzs6IH4xODLBw"
+    Zsecurity    = "UCVPjtOVcnKaSRI8IO3KSetA"
+    Sasquach     = "UCUoJk48pujh29p8zLsnD5PQ"
+}
+
 
 function Get-ChannelID {
     param (
@@ -53,7 +63,7 @@ function newVideo {
  )
 
 # ----------------------------------------------------------------------------------------------
-Invoke-RestMethod $channelPage | Invoke-Expression
+#Invoke-RestMethod $channelPage | Invoke-Expression
 
 $channelID = $channels.$creator
 # ----------------------------------------------------------------------------------------------
